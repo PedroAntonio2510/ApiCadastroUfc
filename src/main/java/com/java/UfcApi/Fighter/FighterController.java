be2b96ab8002a65897d08135f868210efc9daf3d
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-23 20:36:23 -0300

    feat: Mock the routes POST, GET and GET/id

--- src/main/java/com/java/UfcApi/Fighter/FighterController.java
+++ src/main/java/com/java/UfcApi/Fighter/FighterController.java
@@ -1,5 +1,36 @@
 package com.java.UfcApi.Fighter;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fighter")
 public class FighterController {
-    
+
+    private final FighterService fighterService;
+
+    public FighterController(FighterService fighterService) {
+        this.fighterService = fighterService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody FighterDTO fighter) {
+        FighterModel newFighter = fighterService.save(fighter);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Fighter added sucesslly: " + newFighter.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FighterDTO>> list() {
+        List<FighterDTO> fighters = fighterService.listAll();
+        return ResponseEntity.ok(fighters);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FighterDTO> listById(@PathVariable Long id) {
+        FighterDTO figherId = fighterService.listById(id);
+        return ResponseEntity.ok(figherId);
+    }
 }
