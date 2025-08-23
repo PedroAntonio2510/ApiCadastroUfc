fd2bb9bbd521aa3356596e02a1681d5e2d3fb480
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-23 13:37:13 -0300

    feat: Add FighterRepository to extens JpaRepository

--- src/main/java/com/java/UfcApi/Fighter/CATEGORY.java
+++
@@ -1,5 +0,0 @@
-package com.java.UfcApi.Fighter;
-
-public enum CATEGORY {
-    
-}
--- src/main/java/com/java/UfcApi/Fighter/FighterRepository.java
+++ src/main/java/com/java/UfcApi/Fighter/FighterRepository.java
@@ -2,6 +2,6 @@ package com.java.UfcApi.Fighter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FighterRepository {
+public interface FighterRepository extends JpaRepository<FighterModel, Long>{
     
 }
