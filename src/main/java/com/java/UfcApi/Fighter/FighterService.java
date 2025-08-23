4749402708da1df9a984e0772acb7e410a07bb37
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-23 20:36:00 -0300

    feat: Create the method to save, list and listbyId on the service

--- src/main/java/com/java/UfcApi/Fighter/FighterService.java
+++ src/main/java/com/java/UfcApi/Fighter/FighterService.java
@@ -2,7 +2,35 @@ package com.java.UfcApi.Fighter;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FighterService {
-    
+
+    private final FighterRepository repository;
+    private final FighterMapper mapper;
+
+    public FighterService(FighterRepository repository, FighterMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public FighterModel save(FighterDTO fighterDTO) {
+        FighterModel newFighter = mapper.map(fighterDTO);
+        return this.repository.save(newFighter);
+    }
+
+    public List<FighterDTO> listAll() {
+        List<FighterModel> fighters = this.repository.findAll();
+        return fighters.stream()
+                .map(mapper::map)
+                .toList();
+    }
+
+    public FighterDTO listById(Long id) {
+        Optional<FighterModel> fighter = this.repository.findById(id);
+        return fighter.map(mapper::map).orElse(null);
+    }
+
 }
