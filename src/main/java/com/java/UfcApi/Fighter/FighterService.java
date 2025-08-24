ac9fcee633a209382c692154b16b598459fe41a8
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-23 21:27:19 -0300

    feat: Add the methods to update and delete fighters

--- src/main/java/com/java/UfcApi/Fighter/FighterService.java
+++ src/main/java/com/java/UfcApi/Fighter/FighterService.java
@@ -33,4 +33,30 @@ public class FighterService {
         return fighter.map(mapper::map).orElse(null);
     }
 
+    public void delete(Long id) {
+        this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Fighter not found"));
+        this.repository.deleteById(id);
+    }
+
+    public FighterDTO update(Long id, FighterDTO fighterDTO) {
+        return repository.findById(id)
+                .map(fighter -> {
+                    fighter.setName(fighterDTO.name());
+                    fighter.setCpf(fighterDTO.cpf());
+                    fighter.setNickname(fighterDTO.nickname());
+                    fighter.setWeight(fighterDTO.weight());
+                    fighter.setHeight(fighterDTO.height());
+                    fighter.setWeightDivision(fighterDTO.weightDivision());
+                    fighter.setNumberVictory(fighterDTO.numberVictory());
+                    fighter.setNumberLose(fighterDTO.numberLose());
+                    fighter.setNumberDraw(fighterDTO.numberDraw());
+                    this.repository.save(fighter);
+                    return this.mapper.map(fighter);
+                }).orElseThrow(() -> {
+                    return new IllegalArgumentException("Fighter not found");
+                });
+    }
+
+
 }
