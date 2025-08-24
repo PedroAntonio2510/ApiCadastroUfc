94807ac1d7954b2acda29cd3ae380d8a3ad626ca
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-24 12:16:14 -0300

    feat: Add EventRepository to sue JPA

---
+++ src/main/java/com/java/UfcApi/Event/EventRepository.java
@@ -0,0 +1,6 @@
+package com.java.UfcApi.Event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<EventModel, Long> {
+}
---
+++ src/main/java/com/java/UfcApi/Fight/CreateFightDTO.java
@@ -0,0 +1,4 @@
+package com.java.UfcApi.Fight;
+
+public record CreateFightDTO() {
+}
