3cf4e8c4c176cd89dc302ecc19c057c8a69abe0a
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-29 18:11:04 -0300

    feat: Add the EventDTo and EventMapper to remove the responsability from the EventModel

--- src/main/java/com/java/UfcApi/Event/EventDTO.java
+++ src/main/java/com/java/UfcApi/Event/EventDTO.java
@@ -1,4 +1,14 @@
 package com.java.UfcApi.Event;
 
-public record EventDTO() {
+import com.java.UfcApi.Fight.FightDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record EventDTO(
+        String name,
+        String eventType,
+        LocalDate eventDate,
+        List<FightDTO> fights
+) {
 }
--- src/main/java/com/java/UfcApi/Event/EventMapper.java
+++ src/main/java/com/java/UfcApi/Event/EventMapper.java
@@ -1,4 +1,44 @@
 package com.java.UfcApi.Event;
 
+import com.java.UfcApi.Fight.FightMapper;
+import com.java.UfcApi.Fight.FightModel;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class EventMapper {
+
+    private final FightMapper fightMapper;
+
+    public EventMapper(FightMapper fightMapper) {
+        this.fightMapper = fightMapper;
+    }
+
+    public EventModel map(EventDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+        EventModel eventModel = new EventModel();
+        eventModel.setName(dto.name());
+        eventModel.setEventType(dto.eventType());
+        eventModel.setEventDate(dto.eventDate());
+        eventModel.setFights(dto.fights().stream()
+                .map(fightMapper::map)
+                .toList());
+
+        return eventModel;
+    }
+
+    public EventDTO map(EventModel model) {
+        EventDTO eventDTO = new EventDTO(
+                model.getName(),
+                model.getEventType(),
+                model.getEventDate(),
+                model.getFights().stream()
+                        .map(fightMapper::map)
+                        .toList()
+        );
+        return eventDTO;
+    }
 }
